0a19a2b255ca1e62ee070a7b6db696aafcb9ad99
kduytran443 <trankhanhduy18@gmail.com>
2025-02-25 08:41:30 +0700

    Using filterNo, map, flatMap

--- greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
+++ greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -2,10 +2,13 @@ package com.learnkafkastreams.topology;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Arrays;
 
 @Slf4j
 public class GreetingsTopology {
@@ -22,7 +25,14 @@ public class GreetingsTopology {
         greetingsStream.peek((key, value) -> log.info("[GREETING] {}: {}", key, value));
 
         // Processing logic
-        var modifiedStream = greetingsStream.mapValues((readOnlyKey, value) -> value.toUpperCase());
+        var modifiedStream = greetingsStream
+                .peek((key, value) -> log.info("Skip = {}", value.toUpperCase().equals(value)))
+                .filterNot((key, value) -> value.toUpperCase().equals(value)) // Skip already Upper text
+                .map((readOnlyKey, value) -> KeyValue.pair(readOnlyKey.toUpperCase(), value.toUpperCase()))
+                .flatMap((key, value) -> {
+                    var newValues = Arrays.asList(value.split(""));
+                    return newValues.stream().map(val -> KeyValue.pair(key, val)).toList();
+                });
 
         // Sink processor
         modifiedStream.to(GREETING_UPPERCASE, Produced.with(Serdes.String(), Serdes.String()));

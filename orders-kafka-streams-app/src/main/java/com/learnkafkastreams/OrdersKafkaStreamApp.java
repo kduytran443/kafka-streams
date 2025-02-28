32682e9a2141436fe2e6c27968151a8d84dc7d54
kduytran443 <trankhanhduy18@gmail.com>
2025-02-28 23:59:29 +0700

    Kafka stream order flow

--- orders-kafka-streams-app/src/main/java/com/learnkafkastreams/OrdersKafkaStreamApp.java
+++ orders-kafka-streams-app/src/main/java/com/learnkafkastreams/OrdersKafkaStreamApp.java
@@ -1,11 +1,14 @@
 package com.learnkafkastreams;
 
 
+import com.learnkafkastreams.topology.OrdersTopology;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 
 import java.util.List;
 import java.util.Properties;
@@ -14,52 +17,50 @@ import java.util.stream.Collectors;
 @Slf4j
 public class OrdersKafkaStreamApp {
 
-
-    public static void main(String[] args) {
-
-        // create an instance of the topology
-
-
-        Properties config = new Properties();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "orders-app"); // consumer group
-        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest"); // read only the new messages
-        //createTopics(config, List.of(GREETINGS, GREETINGS_UPPERCASE, GREETINGS_SPANISH));
-
-        //Create an instance of KafkaStreams
-        //var kafkaStreams = new KafkaStreams(topology, config);
-
-        //This closes the streams anytime the JVM shuts down normally or abruptly.
-        //Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
-        try{
-            //kafkaStreams.start();
-        }catch (Exception e ){
-            log.error("Exception in starting the Streams : {}", e.getMessage(), e);
-        }
-
+    private static Properties getProperties() {
+        Properties boostrapProperties = new Properties();
+        boostrapProperties.put(StreamsConfig.APPLICATION_ID_CONFIG, "greetings-stream-app");
+        boostrapProperties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        boostrapProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return boostrapProperties;
     }
 
-    private static void createTopics(Properties config, List<String> greetings) {
+    public static void main(String[] args) {
+        log.info("CPU Core: {}", Runtime.getRuntime().availableProcessors());
 
+        Properties boostrapProperties = getProperties();
+        createTopics(boostrapProperties, List.of(OrdersTopology.TOPIC_ORDERS, OrdersTopology.TOPIC_STORES));
+
+        Topology topology = OrdersTopology.buildTopology();
+
+        // Create an instance of KafkaStreams
+        var kafkaStreams = new KafkaStreams(topology, boostrapProperties);
+
+        // This closes the streams anytime the JVM shuts down normally or abruptly.
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+        try {
+            kafkaStreams.start();
+        } catch (Exception e ) {
+            log.error("Exception in starting the Streams : {}", e.getMessage(), e);
+        }
+    }
+
+    private static void createTopics(Properties config, List<String> topics) {
         AdminClient admin = AdminClient.create(config);
         var partitions = 1;
         short replication  = 1;
 
-        var newTopics = greetings
+        var newTopics = topics
                 .stream()
-                .map(topic ->{
-                    return new NewTopic(topic, partitions, replication);
-                })
+                .map(topic -> new NewTopic(topic, partitions, replication))
                 .collect(Collectors.toList());
 
         var createTopicResult = admin.createTopics(newTopics);
         try {
-            createTopicResult
-                    .all().get();
+            createTopicResult.all().get();
             log.info("topics are created successfully");
         } catch (Exception e) {
             log.error("Exception creating topics : {} ",e.getMessage(), e);
         }
     }
-
 }

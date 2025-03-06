be1d545d972514b8b2dd5e530fa515166f2976d4
kduytran443 <trankhanhduy18@gmail.com>
2025-03-07 02:35:13 +0700

    Make total revenue model

--- orders-kafka-streams-app/src/main/java/com/learnkafkastreams/domain/TotalRevenue.java
+++ orders-kafka-streams-app/src/main/java/com/learnkafkastreams/domain/TotalRevenue.java
@@ -3,6 +3,16 @@ package com.learnkafkastreams.domain;
 import java.math.BigDecimal;
 
 public record TotalRevenue(String locationId,
-                           Integer runnuingOrderCount,
+                           Integer runningOrderCount,
                            BigDecimal runningRevenue) {
+
+    public TotalRevenue() {
+        this(null, 0, BigDecimal.ZERO);
+    }
+
+    public TotalRevenue update(String key, Order order) {
+        var newCount = runningOrderCount + 1;
+        var newRevenue = this.runningRevenue.add(order.finalAmount());
+        return new TotalRevenue(key, newCount, newRevenue);
+    }
 }
